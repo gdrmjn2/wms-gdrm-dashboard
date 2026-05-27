f37aca1f8e28a0f0204671f35855eca78db4ab14
gdrmjn2 <gdrmjn2.1@gmail.com>
2026-05-27 11:23:08 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../lib/supabase";
 import { Moon, Sun, Search, Lock } from "lucide-react";
 
 export default function Home() {
